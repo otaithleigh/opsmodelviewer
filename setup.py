bfff2776a4a05c76e7d16b16baefefdd34635b2c
Peter Talley <peterctalley@gmail.com>
2019-06-11 19:33:40 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+with open('README.rst') as file:
+    long_description = file.read()
+
+setup(
+    name='opsmodelviewer',
+    version='0.1.0',
+
+    description='A viewer for 2D OpenSees models.',
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+
+    package_dir={'': 'src'},
+    py_modules=['opsmodelviewer'],
+    entry_points={
+        'console_scripts': ['opsmodelviewer = opsmodelviewer:main']
+    },
+
+    python_requires='>=3.5',
+    install_requires=['bokeh'],
+
+    author='Peter Talley',
+    author_email='ptalley2@vols.utk.edu',
+    url='https://github.com/otaithleigh/opsmodelviewer',
+
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Scientific/Engineering :: Visualization'
+    ],
+)
