33fb3c932a3bc5e76cfc6850443b230ce4d16807
Peter Talley <peterctalley@gmail.com>
2019-06-20 16:40:25 -0400

    Bump version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst') as file:
 
 setup(
     name='opsmodelviewer',
-    version='0.1.0',
+    version='0.2.0',
 
     description='A viewer for 2D OpenSees models.',
     long_description=long_description,
