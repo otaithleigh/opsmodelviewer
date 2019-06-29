8973c4ce5d7646c66ef4965ade09fe51542e4349
Peter Talley <peterctalley@gmail.com>
2019-06-29 15:28:45 -0400

    Update examples for new color stuff

--- examples/strongback.py
+++ examples/strongback.py
@@ -1,5 +1,7 @@
 import enum
 import os
+
+import bokeh.palettes
 from opsmodelviewer import Model
 
 kinds = {
@@ -19,21 +21,28 @@ def kind(v):
     return kinds[v]
 
 
-colors = {
-    'LEFT': '#a6cee3',
-    'RIGHT': '#1f78b4',
-    'LEAN': '#b2df8a',
-    'BEAM': '#33a02c',
-    'BRACE': '#fb9a99',
-    'SBACK': '#e31a1c',
-    'TIE': '#fdbf6f',
-    'RIGID': '#ff7f00',
-    'SPRING': '#cab2d6',
-}
+# Colors are specified by hex codes. bokeh contains various palettes built-in,
+# which can save you the effort of looking them up.
+colors = bokeh.palettes.Category10[len(kinds)]
+
+# The tag spec defines how your tags are structured. Each digit is assigned to
+# a field; the names are not important. For example, a tag processed according
+# to the spec below would have one digit assigned to `kind`, one to `story`, and
+# two to `num`. So 2210 is the tag for kind 2, story 2, num 10. Missing digits
+# filled with zeroes; 210 is the tag for kind 0, story 2, num 10.
+tag_spec = ['kind', 'story', 'num', 'num']
+
+# The tag mapping takes the integers parsed from the tag and applies a function
+# to transform them into something else. In this case, integers are passed to
+# the `kind` function, which returns a string depending on which integer was
+# received. The only limitation is that the result of these mappings must be
+# JSON serializable -- that is, not an object that can't be represented in text.
+tag_mapping = {'kind': kind}
 
 ROOT = os.path.abspath(os.path.dirname(__file__))
 model = Model.from_json(os.path.join(ROOT, 'strongback.json'),
-                        ['kind', 'story', 'num', 'num'], {'kind': kind},
+                        spec=tag_spec,
+                        mapping=tag_mapping,
                         colorkey='kind',
-                        colormap=colors)
-model.show('strongback.html')
+                        palette=colors)
+model.show(os.path.join(ROOT, 'strongback.html'))
